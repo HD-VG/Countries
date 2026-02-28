0830b7a0f723603afb072669b8975ef30421f517
HD-VG <vargashectordanielgonzales@gmail.com>
2026-02-28 16:49:48 -0400

    fix test dto

--- backend/src/model/map.dto.spec.js
+++ backend/src/model/map.dto.spec.js
@@ -1,4 +1,6 @@
 import { mapa } from './map.dto.js';
+import * as chai from 'chai';
+const expect = chai.expect;   
 
 describe('Map DTO', () => {
   it('debe transformar los datos de la API al formato correcto', () => {
@@ -8,17 +10,19 @@ describe('Map DTO', () => {
     
     const resultado = mapa(apiData);
     
-    expect(resultado[0]).toEqual({
+    expect(resultado[0]).to.deep.equal({
       year: 1961,
       carbon: 2.5,
       country: 'Ecuador'
     });
-    expect(resultado[0]).not.toHaveProperty('extra');
+    
+    expect(resultado[0]).to.not.have.property('extra');
   });
 
   it('debe poner carbon en 0 si viene nulo', () => {
     const apiData = [{ year: 2022, carbon: null, countryName: 'Test' }];
     const resultado = mapa(apiData);
-    expect(resultado[0].carbon).toBe(0);
+
+    expect(resultado[0].carbon).to.equal(0);
   });
 });
